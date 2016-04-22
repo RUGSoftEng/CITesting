615a622132175d5fba137182fce2205864fd85fb
Laura Baakman <laurabaakman@gmail.com>
2016-04-22 17:56:55 +0200

    Moved moveTwo to its own module.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,3 @@
 define([], function () {
 
-  function moveTwo(otherTerm){
-    if(otherTerm < 0) result = otherTerm -2;
-    else result = otherTerm + 2;
-    return result;
-  }
-
-  return {
-    moveTwo: moveTwo,
-  };
-
 });
