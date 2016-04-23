c83a32af2b44a0636571879bfedbfa93f9a94b7b
Laura Baakman <laurabaakman@gmail.com>
2016-04-23 09:33:24 +0200

    We don't need bootstrap.

--- app/common.js
+++ app/common.js
@@ -2,8 +2,6 @@ requirejs.config({
     baseUrl: 'node_modules',
     paths: {
         app: "../app",
-        jquery : "jquery/dist/jquery.min",
-        bootstrap :  "../lib/bootstrap.min"
+        jquery : "jquery/dist/jquery.min"
     }
-
 });
